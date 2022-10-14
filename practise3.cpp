3572b394addfc764406b6fe0d2df5b5c226d7a8d
LONGXIXIXI <115448031+LONGXIXIXI@users.noreply.github.com>
2022-10-14 14:35:22 +0800

    Add files via upload

---
+++ practise3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+int main()
+{
+	char arr1[]="welcome to peiking university";
+	char arr2[]="#############################";
+	int left=0;
+	int right=strlen(arr1)-1;
+	for(;left<=right;left++,right--)
+	{
+		arr2[left]=arr1[left];
+		arr2[right]=arr1[right];
+		system("cls");       //执行系统命令的一个函数-cls ：清空屏幕
+		printf("%s\n",arr2);
+		Sleep(1000);        //休息一秒
+		
+	}
+	return 0;
+}
